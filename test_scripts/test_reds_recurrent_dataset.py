a0f38665a830b75c51a370bf68312f2c435e22fd
wscffaa <1294714904@qq.com>
2023-10-20 08:41:19 +0000

    测试REDSRecurrentDataset

--- test_scripts/test_reds_recurrent_dataset.py
+++ test_scripts/test_reds_recurrent_dataset.py
@@ -16,10 +16,10 @@ def main(mode='folder'):
     opt['phase'] = 'train'
 
     opt['name'] = 'REDS'
-    opt['type'] = 'REDSDataset'
+    opt['type'] = 'REDSRecurrentDataset'
     if mode == 'folder':
         opt['dataroot_gt'] = 'datasets/REDS/train/train_sharp'
-        opt['dataroot_lq'] = 'datasets/REDS/train/train_sharp_bicubic/X4'
+        opt['dataroot_lq'] = 'datasets/REDS/train/train_sharp'
         opt['dataroot_flow'] = None
         opt['meta_info_file'] = 'basicsr/data/meta_info/meta_info_REDS_GT.txt'
         opt['io_backend'] = dict(type='disk')
@@ -30,7 +30,7 @@ def main(mode='folder'):
         opt['meta_info_file'] = 'basicsr/data/meta_info/meta_info_REDS_GT.txt'
         opt['io_backend'] = dict(type='lmdb')
 
-    opt['val_partition'] = 'REDS4'
+    opt['val_partition'] = 'official'
     opt['num_frame'] = 5
     opt['gt_size'] = 256
     opt['interval_list'] = [1]
@@ -39,15 +39,17 @@ def main(mode='folder'):
     opt['use_rot'] = True
 
     opt['num_worker_per_gpu'] = 1
-    opt['batch_size_per_gpu'] = 16
-    opt['scale'] = 4
+    opt['batch_size_per_gpu'] = 2
+    opt['scale'] = 1
+
+    opt['test_mode'] = False
 
     opt['dataset_enlarge_ratio'] = 1
 
     os.makedirs('tmp', exist_ok=True)
 
     dataset = build_dataset(opt)
-    data_loader = build_dataloader(dataset, opt, num_gpu=0, dist=opt['dist'], sampler=None)
+    data_loader = build_dataloader(dataset, opt, num_gpu=2, dist=opt['dist'], sampler=None)
 
     nrow = int(math.sqrt(opt['batch_size_per_gpu']))
     padding = 2 if opt['phase'] == 'train' else 0
@@ -57,7 +59,7 @@ def main(mode='folder'):
         if i > 5:
             break
         print(i)
-
+        # 这里的lq和gt都是多组图像，目前lq并未降质
         lq = data['lq']
         gt = data['gt']
         key = data['key']
@@ -65,7 +67,7 @@ def main(mode='folder'):
         for j in range(opt['num_frame']):
             torchvision.utils.save_image(
                 lq[:, j, :, :, :], f'tmp/lq_{i:03d}_frame{j}.png', nrow=nrow, padding=padding, normalize=False)
-        torchvision.utils.save_image(gt, f'tmp/gt_{i:03d}.png', nrow=nrow, padding=padding, normalize=False)
+            torchvision.utils.save_image(gt[:, j, :, :, :], f'tmp/gt_{i:03d}_frame{j}.png', nrow=nrow, padding=padding, normalize=False)
 
 
 if __name__ == '__main__':
